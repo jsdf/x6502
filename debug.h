d265b3b537ae90e28de517e87febf427ec95d28e
James Friend <james@kitefire.com>
2013-12-20 13:44:49 +0800

    always print debug output, debug output to stderr

--- debug.h
+++ debug.h
@@ -3,11 +3,11 @@
 
 #include "cpu.h"
 
-#ifdef DEBUG
+// #ifdef DEBUG
 #define DUMP_DEBUG(cpu) (dump_cpu(cpu))
-#else
-#define DUMP_DEBUG(cpu)
-#endif
+// #else
+// #define DUMP_DEBUG(cpu)
+// #endif
 
 void dump_cpu(cpu *m);
 
